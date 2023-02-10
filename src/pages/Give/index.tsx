fdf24c18db2d4715f8363583c82a5e58dcd50f5a
Jisu Moon <answltn5935@gmail.com>
2023-02-10 18:28:17 +0900

    feat : 주문목록페이지 버튼 클릭시 userId, orderNumber 처리

--- src/pages/Give/index.tsx
+++ src/pages/Give/index.tsx
@@ -1,13 +1,18 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 
 const Give = () => {
   useEffect(() => {
-    window.addEventListener('message', event => {
-      if (event.origin !== process.env.REACT_APP_MESSEAGE_ORIGIN) return;
-      console.log(event.data);
+    window.addEventListener('message', async event => {
+      // if (event.origin !== process.env.REACT_APP_MESSEAGE_ORIGIN) return;
+      const { orderNumber, userId } = event.data;
+      await axios.post('https://itsus.co.kr:5555/api/imall/giveCoin', {
+        orderNumber,
+        userId,
+      });
     });
   }, []);
-  return <></>;
+  return null;
 };
 
 export default Give;
