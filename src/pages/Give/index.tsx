cbc2c44039b044c7f43290e47b8cb5be03ec60a4
Jisu Moon <answltn5935@gmail.com>
2023-02-15 15:39:44 +0900

    fix : message 잘못 넘어올때 우회

--- src/pages/Give/index.tsx
+++ src/pages/Give/index.tsx
@@ -1,18 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Give = () => {
+  const [coinAmount, setCoinAmount] = useState<number | boolean>(0);
   useEffect(() => {
     window.addEventListener('message', async event => {
       // if (event.origin !== process.env.REACT_APP_MESSEAGE_ORIGIN) return;
       const { orderNumber, userId, type } = event.data;
-      await axios.post('https://itsus.co.kr:5555/api/imall/giveCoin', {
-        orderNumber,
-        userId,
-        type,
-      });
+      if (!orderNumber && !userId && type === 'webpackClose') return;
+      const result = await axios.post(
+        'https://itsus.co.kr:5555/api/imall/giveCoin',
+        {
+          orderNumber,
+          userId,
+          type,
+        },
+      );
+      setCoinAmount(result.data);
     });
   }, []);
+  console.log(coinAmount);
   return null;
 };
 
