7c24de165ad796df51382439bfaa15dd4a27cae7
Jisu Moon <answltn5935@gmail.com>
2023-06-01 16:01:02 +0900

    feat : give modal에 날짜 선택 추가

--- src/pages/Main/Give/Modal/index.tsx
+++ src/pages/Main/Give/Modal/index.tsx
@@ -3,6 +3,8 @@ import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import * as S from './styled';
 import { IMessage } from '../..';
 import axios from 'axios';
+import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from 'date-fns/esm/locale'; //한국어 설정
 
 interface IProps {
   setModalShow: Dispatch<SetStateAction<boolean>>;
@@ -11,21 +13,50 @@ interface IProps {
 
 export default function GiveCoinModal({ setModalShow, message }: IProps) {
   const [orders, setOrders] = useState<any>([]);
-  console.log(orders);
+  const [date, setDate] = useState<Date>(new Date());
+
+  const formatDateToYYYYMMDD = (date: Date) => {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  };
+
+  const getOrdersHandler = async () => {
+    if (!message?.userId) return;
+    const formatDate = formatDateToYYYYMMDD(date);
+    const result = await axios.post(
+      'https://itsus.co.kr:5555/api/imall/giveCoinTest',
+      { ...message, date: formatDate },
+    );
+    setOrders(result.data);
+  };
   useEffect(() => {
-    const getCoin = async () => {
-      const result = await axios.post(
-        'https://itsus.co.kr:5555/api/imall/giveCoinTest',
-        message,
-      );
-      setOrders(result.data);
-    };
+    const getCoin = async () => {};
     if (message!.userId) {
       getCoin();
     }
   }, [message]);
   return (
     <S.Wrapper>
+      <S.DateWrapper>
+        <div className='date_contents'>
+          <S.DatePickerInput
+            selected={date}
+            shouldCloseOnSelect
+            onChange={(date: Date) => setDate(date)}
+            locale={ko}
+            minDate={new Date('2023-03-01')}
+            maxDate={new Date()}
+            dateFormat='yyyy년 MM월 dd일  ▼'
+          />
+          <S.DateButton onClick={() => getOrdersHandler()}>조회</S.DateButton>
+        </div>
+        <span>
+          선택하신 날짜 기준 3개월 주문목록을 자동조회 후 마음이 지급됩니다.
+        </span>
+      </S.DateWrapper>
       <S.CheckButton onClick={() => setModalShow(false)}>닫기</S.CheckButton>
     </S.Wrapper>
   );
--- src/pages/Main/Give/Modal/styled.ts
+++ src/pages/Main/Give/Modal/styled.ts
@@ -1,3 +1,4 @@
+import DatePicker from 'react-datepicker';
 import styled from 'styled-components';
 
 export const Wrapper = styled.section`
@@ -24,4 +25,39 @@ export const CheckButton = styled.button`
   color: #fff;
   border: 0;
   border-radius: 0 0 15px 15px;
+`;
+
+export const DateWrapper = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  & > span {
+    font-size: 13px;
+    letter-spacing: -0.5px;
+    color: #999;
+    word-break: keep-all;
+  }
+  .date_contents {
+    margin-bottom: 5px;
+    display: flex;
+    gap: 12px;
+  }
+`;
+
+export const DateButton = styled.button`
+  box-sizing: border-box;
+  border: 0;
+  flex: 1 1 40%;
+  border-radius: 5px;
+`;
+
+export const DatePickerInput = styled(DatePicker)`
+  width: 100%;
+  border: 0;
+  color: #fff;
+  background: var(--red);
+  position: relative;
+  box-sizing: border-box;
+  padding: 10px 5px;
+  text-align: center;
+  border-radius: 5px;
 `;
